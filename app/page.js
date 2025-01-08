2a25a70902f3751878bab64611523fad2c94345c
DainLee41 <amydain414@gmail.com>
2025-01-08 21:10:17 +0900

    feat -followed the tutorial making navBar, Header and About page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 'use client'
+import About from "./component/About";
+import Header from "./component/Header";
 import NavBar from "./component/NavBar";
 
 export default function Home() {
   return (
     <>
     <NavBar />
+    <Header />
+    <About />
     </>
   );
 }
