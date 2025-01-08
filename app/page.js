aa33dbc42bb0434ab2427d906699fe9aba984284
DainLee41 <amydain414@gmail.com>
2025-01-08 23:44:30 +0900

    feat -다크모드 적용 완료!

--- app/page.js
+++ app/page.js
@@ -6,17 +6,40 @@ import Work from "./component/Work";
 import Services from "./component/Services";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
+import { use, useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true)
+    } else {
+      setIsDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=> {
+    if (isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+
+  },[isDarkMode])
+
   return (
     <>
-    <NavBar />
-    <Header />
-    <About />
-    <Services />
-    <Work />
-    <Contact />
-    <Footer />
+    <NavBar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+    <Header isDarkMode={isDarkMode}/>
+    <About isDarkMode={isDarkMode}/>
+    <Services isDarkMode={isDarkMode}/>
+    <Work isDarkMode={isDarkMode}/>
+    <Contact isDarkMode={isDarkMode}/>
+    <Footer isDarkMode={isDarkMode}/>
     </>
   );
 }
